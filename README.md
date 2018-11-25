e908f1639fd45c17bfd659b6e51911baed1d741f
Ken Kotch <hello@ken.uno>
2018-11-24 19:54:26 -0700

    update readme

--- README.md
+++ README.md
@@ -4,3 +4,55 @@
 
 # react-native-columns
 User resizable columns component for react native
+
+## Instalation
+```sh
+yarn add react-native-columns```
+OR (npm install does not work consistently)
+```sh
+npm i -S react-native-columns```
+
+## Example
+```js
+import React from 'react'
+import {
+  Text,
+  View
+} from 'react-native'
+import SomeComponent from './SomeComponent'
+import Columns from 'react-native-columns'
+
+export default class Main extends React.Component {
+  leftCol = (
+    <View>
+      <Text>
+        This is the left column
+      </Text>
+    </View>
+  )
+
+  render() {
+    return (
+      <Columns
+        initialLeft={ 0.4 }
+        max={ 85 }
+        min={ 15 }
+        leftCol={ this.leftCol }
+        rightCol={ <SomeComponent /> }
+      />
+    )
+  }
+}```
+
+## Props
+| property    | default                     | type                    | description                           |
+| :---------: |:---------------------------:| :----------------------:| :------------------------------------ |
+| min         |   20                        |            number x/100 | left column minimum % of screen width |
+| max         |   75                        |            number x/100 | left column maximum % of screen width |
+| initialLeft | 0.35                        | decimal between 0 and 1 | initial left col width x/1            |
+| leftCol     | `<Text>Left Column</Text>`  |         react component | react component                       |
+| rightCol    | `<Text>Right Column</Text>` |         react component | react component                       |
+
+
+#### currently only supports full screen width. PR's welcome
+#### supports iOS and Android
--- src/index.js
+++ src/index.js
@@ -11,12 +11,18 @@ import styles from './index.style'
 
 class Columns extends React.Component {
   static propTypes = {
+    initialLeft: PropTypes.number,
     leftCol: PropTypes.element,
+    max: PropTypes.number,
+    min: PropTypes.number,
     rightCol: PropTypes.element
   }
 
   static defaultProps = {
+    initialLeft: 0.35,
     leftCol: <Text>Left Column</Text>,
+    max: 75,
+    min: 20,
     rightCol: <Text>Right Column</Text>
   }
 
@@ -25,12 +31,14 @@ class Columns extends React.Component {
     this.state = {
       height: Dimensions.get('window').height,
       leftColumnWidth: Dimensions.get('window').width * 0.35,
-      max: Dimensions.get('window').width * 0.75,
-      min: Dimensions.get('window').width * 0.2,
       rightColumnWidth: Dimensions.get('window').width * 0.65,
       start: Dimensions.get('window').width * 0.35,
       width: Dimensions.get('window').width
     }
+  }
+
+  componentDidMount() {
+    this.widthChange((this.state.width * this.props.initialLeft))
   }
 
   widthChange = left => {
@@ -42,48 +50,50 @@ class Columns extends React.Component {
   }
 
   render() {
-      const {
-        height,
-        leftColumnWidth,
-        max,
-        min,
-        start,
-        width
-      } = this.state
-      return (
-        <View style={ styles.container }>
+    const {
+      max,
+      min
+    } = this.props
+    const {
+      height,
+      leftColumnWidth,
+      start,
+      width
+    } = this.state
+    return (
+      <View style={ styles.container }>
 
-          {/* Width Control */}
-          <View style={ [styles.sliderContainer, { width: width * 0.65 }] }>
-            <Slider
-              maximumValue={ max }
-              maximumTrackTintColor="transparent"
-              minimumValue={ min }
-              minimumTrackTintColor="transparent"
-              onValueChange={ value => this.widthChange(value) }
-              step={ 0 }
-              style={ [styles.slider, { paddingTop: height + height }] }
-              thumbTintColor="transparent"
-              value={ start }
-            />
+        {/* Width Control */}
+        <View style={ [styles.sliderContainer, { width: width * 0.65 }] }>
+          <Slider
+            maximumValue={ max }
+            maximumTrackTintColor="transparent"
+            minimumValue={ min }
+            minimumTrackTintColor="transparent"
+            onValueChange={ value => this.widthChange(value) }
+            step={ 0 }
+            style={ [styles.slider, { paddingTop: height + height }] }
+            thumbTintColor="transparent"
+            value={ start }
+          />
+        </View>
+
+        <View style={ [styles.columnContainer, { top: -height - height }] }>
+
+          {/* Left Column */}
+          <View style={ styles.leftColumnContainer }>
+            { this.props.leftCol }
           </View>
 
-          <View style={ [styles.columnContainer, { top: -height - height }] }>
+          {/* Center Column (mid-line) */}
+          <View style={ styles.centerColumn } />
 
-            {/* Left Column */}
-            <View style={ styles.leftColumnContainer }>
-              { this.props.leftCol }
-            </View>
-
-            {/* Center Column (mid-line) */}
-            <View style={ styles.centerColumn } />
-
-            {/* Right Column */}
-            <View style={ [styles.rightColumn, { width: this.state.rightColumnWidth }] }>
-              { this.props.rightCol }
-            </View>
+          {/* Right Column */}
+          <View style={ [styles.rightColumn, { width: this.state.rightColumnWidth }] }>
+            { this.props.rightCol }
           </View>
         </View>
+      </View>
     )
   }
 }
